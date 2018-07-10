012e20c8754600ff121597055230378a8cd48c65
Alireza Tajadod <Atajadod94@gmail.com>
2018-07-10 13:57:13 -0700

    assignment fixes

--- core_database_concepts/movie_database/solution.sql
+++ core_database_concepts/movie_database/solution.sql
@@ -11,38 +11,42 @@ CREATE TABLE movies (
 );
 
 CREATE TABLE taginfo (
-    tagid INTEGER,
-    content TEXT,
+    tagid INTEGER NOT NULL,
+    content TEXT NOT NULL,
 );
 
 CREATE TABLE genres (
-    genreid INTEGER,
-    name TEXT,
+    genreid INTEGER NOT NULL,
+    name TEXT NOT NULL,
     PRIMARY KEY (genreid)
 );
 
 CREATE TABLE ratings (
-    userid INTEGER,
-    movieid INTEGER,
-    rating NUMERIC,
-    timestamp BIGINT,
+    userid INTEGER NOT NULL,
+    movieid INTEGER NOT NULL,
+    rating NUMERIC NOT NULL,
+    timestamp BIGINT NOT NULL,
+    PRIMARY KEY (userid, movieid),
     FOREIGN KEY (userid) REFERENCES users(userid),
-    FOREIGN KEY (movieid) REFERENCES movies(movieid)
+    FOREIGN KEY (movieid) REFERENCES movies(movieid),
+    CONSTRAINT rating CHECK (rating>=0 AND rating<=5)
 );
 
 CREATE TABLE tags (
-    userid INTEGER,
-    movieid INTEGER,
-    tagid INTEGER,
+    userid INTEGER NOT NULL,
+    movieid INTEGER NOT NULL,
+    tagid INTEGER NOT NULL,
     timestamp BIGINT,
+    PRIMARY KEY (userid, movieid, tagid),
     FOREIGN KEY (userid) REFERENCES users(userid),
     FOREIGN KEY (movieid) REFERENCES movies(movieid),
     FOREIGN KEY (tagid) REFERENCES taginfo(tagid)
 );
 
 CREATE TABLE hasagenre (
-  movieid INTEGER,
-  genreid INTEGER,
+  movieid INTEGER NOT NULL,
+  genreid INTEGER NOT NULL,
+  PRIMARY KEY (movieid, genreid),
   FOREIGN KEY (movieid) REFERENCES movies(movieid),
-  FOREIGN KEY (genreid) REFERENCES genres(genreid),
+  FOREIGN KEY (genreid) REFERENCES genres(genreid)
 );
